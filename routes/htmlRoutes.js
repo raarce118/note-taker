5d5d1017d0988b51858c99afe4bf4a30e7621006
Rich Arce <raarce118@gmail.com>
2022-04-28 21:00:23 -0700

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get("/notes", (req, res) => {
-     res.sendFile(path.join(_dirname, "../public/notes/notes.html"))
+     res.sendFile(path.join(_dirname, "../public/notes.html"))
 })
 
 module.exports = router;
